691927fa5b7698505cd7019cebae81461788764f
Dharaneesh-2005 <kit27.ad11@gmail.com>
2026-03-04 16:21:52 +0530

    Add publicDir to vite config for PWA manifest

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: 'public',
   server: {
     host: '0.0.0.0', // Listen on all network interfaces
     port: 5173, // Vite default - different from proxy server (3000)
